ec8c33e4143b25e924820432a3089ab70cb3a80c
ICS-ShanghaiTech <tracer@ics>
2022-09-27 19:50:14 +0800

    qixing qixing

--- projects/lab2/vsrc/src/top.v
+++ projects/lab2/vsrc/src/top.v
@@ -14,9 +14,11 @@ module top#(
 );
 
 reg rst;
+wire [DATA_WIDTH*N-1:0] X_pipe;
 reg [DATA_WIDTH*N-1:0] X;
 reg [DATA_WIDTH*N*K-1:0] W;
-reg [DATA_WIDTH*K-1:0] Y;
+wire [DATA_WIDTH*K-1:0] Y_pipe;
+wire [DATA_WIDTH*K-1:0] Y;
 
 systolic_array #(
     .M(M),
@@ -30,6 +32,34 @@ systolic_array #(
     .W(W),
     .Y(Y)
 );
+
+genvar i;
+
+assign X_pipe[DATA_WIDTH-1:0] = X[DATA_WIDTH-1:0];
+for(i = 1; i < N; i = i + 1) begin
+    pipe#(
+        .DATA_WIDTH(DATA_WIDTH),
+        .pipes(i)
+    )pipes_X(
+        .clk(clk),
+        .rst(rst),
+        .in_p(X[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH]),
+        .out_p(X_pipe[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH])
+    );
+end
+
+assign Y_pipe[DATA_WIDTH-1:0] = Y[DATA_WIDTH-1:0];
+for(i = 1; i < K; i = i + 1) begin
+    pipe#(
+        .DATA_WIDTH(DATA_WIDTH),
+        .pipes(i)
+    )pipes_Y(
+        .clk(clk),
+        .rst(rst),
+        .in_p(Y[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH]),
+        .out_p(Y_pipe[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH])
+    );
+end
 
 reg [31:0] rst_cyc;
 
