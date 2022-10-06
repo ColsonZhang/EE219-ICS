46edfd5cc874cefc4419585d85d099f9cdc749ea
ICS-ShanghaiTech <tracer@ics>
2022-10-06 18:15:24 +0800

    qixing qixing

---
+++ projects/lab2/vsrc/sim/top.v
@@ -0,0 +1,136 @@
+`timescale 1ns / 1ps
+`define IMG_W 4
+`define IMG_H 5
+`define FILTER_NUM 5
+`define FILTER_SIZE 3
+`define DEBUG 0
+
+module pe_top_row(
+    input clk
+);
+
+parameter IMG_H = `IMG_H;
+parameter IMG_W = `IMG_W;
+parameter FILTER_NUM = `FILTER_NUM;
+parameter FILTER_SIZE = `FILTER_SIZE;
+parameter M = `IMG_H * `IMG_W;
+parameter N = `FILTER_SIZE * `FILTER_SIZE;
+parameter K = `FILTER_NUM;
+parameter DATA_WIDTH = 32;
+parameter ADDR_WIDTH = 32;
+
+parameter MEM_SIZE = 32'h00005000;
+parameter IMG_BASE = 32'h00000000;
+parameter WEIGHT_BASE = 32'h00001000;
+parameter IM2COL_BASE = 32'h00002000;
+parameter OUTPUT_BASE = 32'h00003000;
+
+reg rst;
+reg [DATA_WIDTH*N-1:0] X;
+reg [DATA_WIDTH*K-1:0] Y;
+reg [DATA_WIDTH*N*K-1:0] W;
+
+reg [DATA_WIDTH*N-1:0] X_buffer [0:M];
+reg [DATA_WIDTH*K-1:0] Y_buffer [0:M];
+
+wire [ADDR_WIDTH-1:0] addr_rd, addr_wr;
+reg [DATA_WIDTH-1:0] mem [MEM_SIZE-1:0];
+reg [DATA_WIDTH-1:0] data_rd;
+wire [DATA_WIDTH-1:0] data_wr;
+wire mem_wr_en;
+
+reg [31:0] rst_cyc;
+reg [31:0] clk_cnt;
+reg rst_im2col;
+wire im2col_done;
+reg [31:0] im2col_idle;
+
+im2col #(
+    .IMG_W(IMG_W),
+    .IMG_H(IMG_H),
+    .DATA_WIDTH(DATA_WIDTH),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .FILTER_SIZE(FILTER_SIZE),
+    .IMG_BASE(IMG_BASE),
+    .IM2COL_BASE(IM2COL_BASE)
+)im2col(
+    .clk(clk),
+    .rst_im2col(rst_im2col),
+    .data_rd(data_rd),
+    .data_wr(data_wr),
+    .addr_rd(addr_rd),
+    .addr_wr(addr_wr),
+    .im2col_done(im2col_done),
+    .mem_wr_en(mem_wr_en)
+);
+
+systolic_array #(
+    .M(M),
+    .N(N),
+    .K(K),
+    .DATA_WIDTH(DATA_WIDTH)
+)systolic_array(
+    .clk(clk),
+    .rst(rst),
+    .X(X),
+    .W(W),
+    .Y(Y)
+);
+
+
+integer i0, j0, k0;
+task display_img();
+begin
+    $write("\nimage:\n");
+    for (i0 = 0; i0 < IMG_H; i0 = i0 + 1) begin
+        for (j0 = 0; j0 < IMG_W; j0 = j0 + 1) begin
+            $write("%04h ", mem[IMG_BASE + IMG_W * i0 + j0]);
+        end
+        $write("\n");
+    end
+end
+endtask
+
+task display_im2col();
+begin
+    $write("\nim2col:\n");
+    for (i0 = 0; i0 < N; i0 = i0 + 1) begin
+        for (j0 = 0; j0 < K; j0 = j0 + 1) begin
+            $write("%04h ", mem[IM2COL_BASE + N * j0 + i0]);
+        end
+        $write("\n");
+    end
+end
+endtask
+
+// Memory
+always @(posedge clk) begin
+    data_rd <= mem[addr_rd];
+    if (mem_wr_en) begin
+        mem[addr_wr] <= data_wr;
+    end
+end
+
+
+always @(posedge clk) begin
+    if (im2col_idle == 0) begin
+        rst_im2col <= 0;
+    end
+    else begin
+        im2col_idle <= im2col_idle - 1;
+        rst_im2col <= 1;
+    end
+end
+
+initial begin
+    $readmemh("../mem/mem_init.txt", mem);
+    rst_cyc = 5;
+    im2col_idle = 5;
+end
+
+always@(posedge im2col_done) begin
+    display_img();
+    display_im2col();
+end
+
+endmodule
---
+++ projects/lab2/vsrc/src/im2col.v
@@ -0,0 +1,116 @@
+`timescale 1ns / 1ps
+module im2col #(
+    parameter IMG_W = 8,
+    parameter IMG_H = 8,
+    parameter DATA_WIDTH = 8,
+    parameter ADDR_WIDTH = 32,
+    parameter FILTER_SIZE = 3,
+    parameter IMG_BASE = 16'h0000,
+    parameter IM2COL_BASE = 16'h2000
+) (
+    input clk,
+    input rst_im2col,
+    input [DATA_WIDTH-1:0] data_rd,
+    output [DATA_WIDTH-1:0] data_wr,
+    output [ADDR_WIDTH-1:0] addr_wr,
+    output [ADDR_WIDTH-1:0] addr_rd,
+    output reg im2col_done,
+    output reg mem_wr_en
+);
+
+    reg [DATA_WIDTH-1:0] data_out;
+    reg [ADDR_WIDTH-1:0] addr_wr_a,addr_rd_a;
+    wire [ADDR_WIDTH-1:0] addr_wr_a,addr_rd_a;
+    wire [DATA_WIDTH-1:0] data_out;
+    reg [DATA_WIDTH-1:0] count;
+    
+
+    always @(posedge clk or posedge rst_im2col )
+    begin
+        if (rst_im2col)
+            begin
+              count =0;
+            end
+        else if (count==IMG_W*IMG_H*FILTER_SIZE*FILTER_SIZE)
+            begin
+               count =0;
+            end
+        else 
+            begin
+               count =count+1;
+            end
+    end
+                
+    always @(posedge clk or posedge rst_im2col )
+    begin
+        if (rst_im2col)  
+            begin
+              mem_wr_en <= 0;
+            end
+        else  if(clk==1)
+            begin 
+              mem_wr_en <= 1;
+            end
+        else  if (clk==0)
+            begin 
+              mem_wr_en <= 0;
+            end
+    end
+
+    always @(posedge clk or posedge rst_im2col )
+    begin
+        if (rst_im2col)  
+            begin
+              im2col_done <= 0;
+            end
+        else if (count==IMG_W*IMG_H*FILTER_SIZE*FILTER_SIZE)
+            begin
+              im2col_done <= 1;
+            end
+        else 
+            begin
+              im2col_done <= 0;
+            end
+    end
+
+    always @(posedge clk or posedge rst_im2col )
+    begin
+        if (rst_im2col)
+            addr_wr_a=IM2COL_BASE-1;
+        else 
+            addr_wr_a=addr_wr_a+1;
+    end
+
+    always @(posedge clk or posedge rst_im2col )
+    begin
+        if (rst_im2col)
+        begin
+            addr_rd_a=0;
+        end
+        else  
+        begin
+          addr_rd_a=IMG_BASE+ ((count-1)/(FILTER_SIZE*FILTER_SIZE)+ IMG_W*(((count-1)%(FILTER_SIZE*FILTER_SIZE))/FILTER_SIZE - 1) + ((count-1)%(FILTER_SIZE*FILTER_SIZE))%FILTER_SIZE -1);
+        end
+    end
+
+    always @(posedge clk or posedge rst_im2col )
+    begin
+        if (rst_im2col)
+        begin
+            data_out=0; 
+        end
+        else if(mem_wr_en)
+        begin
+            data_out=data_rd;
+        end
+    end
+
+    assign addr_rd = addr_rd_a;
+    assign addr_wr = addr_wr_a;
+
+    assign data_wr=data_out;
+
+
+
+endmodule
+
