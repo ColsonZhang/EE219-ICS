efd1b36027a984f0018de078f1cdcb85aed2d562
ICS-ShanghaiTech <tracer@ics>
2022-10-06 17:21:15 +0800

    qixing qixing

--- projects/lab2/vsrc/src/systolic_array.v
+++ projects/lab2/vsrc/src/systolic_array.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module systolic_array #(
+module systolic_array#(
     parameter M = 5,
     parameter N = 3,
     parameter K = 4,
@@ -13,91 +13,48 @@ module systolic_array #(
     output reg [DATA_WIDTH*K-1:0] Y
 );
 
-wire [DATA_WIDTH-1:0] x_out_arr[N-1:0][K-1:0];
-wire [DATA_WIDTH-1:0] y_out_arr[N-1:0][K-1:0];
-wire [DATA_WIDTH-1:0] w_arr[N-1:0][K-1:0];
+wire [DATA_WIDTH*N-1:0] X_pipe;
+wire [DATA_WIDTH*K-1:0] Y_pipe;
 
-genvar i, j, k;
+array #(
+    .M(M),
+    .N(N),
+    .K(K),
+    .DATA_WIDTH(DATA_WIDTH)
+)array(
+    .clk(clk),
+    .rst(rst),
+    .X(X_pipe),
+    .W(W),
+    .Y(Y_pipe)
+);
 
-for (i = 0; i < N; i = i + 1) begin
-    for (j = 0; j < K; j = j + 1) begin
-        assign w_arr[i][j] = W[(i*K+j+1)*DATA_WIDTH-1:(i*K+j)*DATA_WIDTH];
-    end
+genvar i;
+
+assign X_pipe[DATA_WIDTH-1:0] = X[DATA_WIDTH-1:0];
+for(i = 1; i < N; i = i + 1) begin
+    pipe#(
+        .DATA_WIDTH(DATA_WIDTH),
+        .pipes(i)
+    )pipes_X(
+        .clk(clk),
+        .rst(rst),
+        .in_p(X[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH]),
+        .out_p(X_pipe[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH])
+    );
 end
 
-for (j = 0; j < K; j = j + 1) begin
-    assign Y[(j+1)*DATA_WIDTH-1:j*DATA_WIDTH] = y_out_arr[N-1][j];
+assign Y[K*DATA_WIDTH-1:(K-1)*DATA_WIDTH] = Y_pipe[K*DATA_WIDTH-1:(K-1)*DATA_WIDTH];
+for(i = 1; i < K; i = i + 1) begin
+    pipe#(
+        .DATA_WIDTH(DATA_WIDTH),
+        .pipes(i)
+    )pipes_Y(
+        .clk(clk),
+        .rst(rst),
+        .in_p(Y_pipe[(K-i)*DATA_WIDTH-1:(K-i-1)*DATA_WIDTH]),
+        .out_p(Y[(K-i)*DATA_WIDTH-1:(K-i-1)*DATA_WIDTH])
+    );
 end
-
-for (i = 0; i < N; i = i + 1) begin
-    for (j = 0; j < K; j = j + 1) begin
-        if (i > 0 && j > 0) begin
-            pe#(
-                .M(M),
-                .N(N),
-                .K(K),
-                .DATA_WIDTH(DATA_WIDTH)
-            )pe_body(
-                .clk(clk),
-                .rst(rst),
-                .w(w_arr[i][j]),
-                .x_in(x_out_arr[i][j-1]),
-                .y_in(y_out_arr[i-1][j]),
-                .x_out(x_out_arr[i][j]),
-                .y_out(y_out_arr[i][j])
-            );
-        end
-        if (i == 0 && j ==0) begin
-            pe#(
-                .M(M),
-                .N(N),
-                .K(K),
-                .DATA_WIDTH(DATA_WIDTH)
-            )pe_upper_left(
-                .clk(clk),
-                .rst(rst),
-                .w(w_arr[i][j]),
-                .x_in(X[DATA_WIDTH-1:0]),
-                .y_in(0),
-                .x_out(x_out_arr[i][j]),
-                .y_out(y_out_arr[i][j])
-            );
-        end
-        if (i > 0 && j == 0) begin
-            pe#(
-                .M(M),
-                .N(N),
-                .K(K),
-                .DATA_WIDTH(DATA_WIDTH)
-            )pe_left_col(
-                .clk(clk),
-                .rst(rst),
-                .w(w_arr[i][j]),
-                .x_in(X[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH]),
-                .y_in(y_out_arr[i-1][j]),
-                .x_out(x_out_arr[i][j]),
-                .y_out(y_out_arr[i][j])
-            );
-        end
-        if (i == 0 && j > 0) begin
-            pe#(
-                .M(M),
-                .N(N),
-                .K(K),
-                .DATA_WIDTH(DATA_WIDTH)
-            )pe_top_row(
-                .clk(clk),
-                .rst(rst),
-                .w(w_arr[i][j]),
-                .x_in(x_out_arr[i][j-1]),
-                .y_in(0),
-                .x_out(x_out_arr[i][j]),
-                .y_out(y_out_arr[i][j])
-            );
-        end
-    end
-end
-
-
 
 endmodule
--- projects/lab2/vsrc/src/top.v
+++ projects/lab2/vsrc/src/top.v
@@ -14,52 +14,69 @@ module top#(
 );
 
 reg rst;
-wire [DATA_WIDTH*N-1:0] X_pipe;
 reg [DATA_WIDTH*N-1:0] X;
-reg [DATA_WIDTH*N*K-1:0] W;
-wire [DATA_WIDTH*K-1:0] Y_pipe;
 reg [DATA_WIDTH*K-1:0] Y;
+reg [DATA_WIDTH*N*K-1:0] W;
 
 systolic_array #(
     .M(M),
     .N(N),
     .K(K),
     .DATA_WIDTH(DATA_WIDTH)
-)array(
+)systolic_array(
     .clk(clk),
     .rst(rst),
-    .X(X_pipe),
+    .X(X),
     .W(W),
-    .Y(Y_pipe)
+    .Y(Y)
 );
 
-genvar i;
+// wire [DATA_WIDTH*N-1:0] X_pipe;
+// reg [DATA_WIDTH*N-1:0] X;
+// reg [DATA_WIDTH*N*K-1:0] W;
+// wire [DATA_WIDTH*K-1:0] Y_pipe;
+// reg [DATA_WIDTH*K-1:0] Y;
 
-assign X_pipe[DATA_WIDTH-1:0] = X[DATA_WIDTH-1:0];
-for(i = 1; i < N; i = i + 1) begin
-    pipe#(
-        .DATA_WIDTH(DATA_WIDTH),
-        .pipes(i)
-    )pipes_X(
-        .clk(clk),
-        .rst(rst),
-        .in_p(X[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH]),
-        .out_p(X_pipe[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH])
-    );
-end
+// systolic_array #(
+//     .M(M),
+//     .N(N),
+//     .K(K),
+//     .DATA_WIDTH(DATA_WIDTH)
+// )array(
+//     .clk(clk),
+//     .rst(rst),
+//     .X(X_pipe),
+//     .W(W),
+//     .Y(Y_pipe)
+// );
 
-assign Y[K*DATA_WIDTH-1:(K-1)*DATA_WIDTH] = Y_pipe[K*DATA_WIDTH-1:(K-1)*DATA_WIDTH];
-for(i = 1; i < K; i = i + 1) begin
-    pipe#(
-        .DATA_WIDTH(DATA_WIDTH),
-        .pipes(i)
-    )pipes_Y(
-        .clk(clk),
-        .rst(rst),
-        .in_p(Y_pipe[(K-i)*DATA_WIDTH-1:(K-i-1)*DATA_WIDTH]),
-        .out_p(Y[(K-i)*DATA_WIDTH-1:(K-i-1)*DATA_WIDTH])
-    );
-end
+// genvar i;
+
+// assign X_pipe[DATA_WIDTH-1:0] = X[DATA_WIDTH-1:0];
+// for(i = 1; i < N; i = i + 1) begin
+//     pipe#(
+//         .DATA_WIDTH(DATA_WIDTH),
+//         .pipes(i)
+//     )pipes_X(
+//         .clk(clk),
+//         .rst(rst),
+//         .in_p(X[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH]),
+//         .out_p(X_pipe[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH])
+//     );
+// end
+
+// assign Y[K*DATA_WIDTH-1:(K-1)*DATA_WIDTH] = Y_pipe[K*DATA_WIDTH-1:(K-1)*DATA_WIDTH];
+// for(i = 1; i < K; i = i + 1) begin
+//     pipe#(
+//         .DATA_WIDTH(DATA_WIDTH),
+//         .pipes(i)
+//     )pipes_Y(
+//         .clk(clk),
+//         .rst(rst),
+//         .in_p(Y_pipe[(K-i)*DATA_WIDTH-1:(K-i-1)*DATA_WIDTH]),
+//         .out_p(Y[(K-i)*DATA_WIDTH-1:(K-i-1)*DATA_WIDTH])
+//     );
+// end
 
 reg [31:0] rst_cyc;
 reg [31:0] clk_cnt;
